9c6d68e94040733b2c4ade14ec0701f0ca7c2b4f
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-23 13:29:04 +0300

    display names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import JSONDATA from './MOCK_DATA.json';
 
 function App() {
-    return <div className=''></div>;
+    return (
+        <div className=''>
+            <input type='text' placeholder='Search...' />
+            {JSONDATA.map((val, key) => {
+                return <div>{val.first_name}</div>;
+            })}
+        </div>
+    );
 }
 
 export default App;
