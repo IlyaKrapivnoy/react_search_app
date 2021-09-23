0aa8c54c378d68059b399f199d7e71a76c1aa032
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-23 13:58:31 +0300

    logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 import JSONDATA from './MOCK_DATA.json';
 
 function App() {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <div className='App'>
             <h1 className='title'>Search Bar App</h1>
-            <input type='text' placeholder='Search here' />
-            {JSONDATA.map((val, key) => {
+            <input
+                type='text'
+                placeholder='Search here'
+                onChange={(e) => {
+                    setSearchTerm(e.target.value);
+                }}
+            />
+            {JSONDATA.filter((val) => {
+                if (searchTerm === '') {
+                    return val;
+                } else if (
+                    val.first_name
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                ) {
+                    return val;
+                }
+            }).map((val, key) => {
                 return (
                     <div className='user' key={key}>
                         <p>{val.first_name}</p>
