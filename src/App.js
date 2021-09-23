281d3842b307feb39d827f26fec2e95e93547e49
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-23 13:44:44 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import JSONDATA from './MOCK_DATA.json';
 
 function App() {
     return (
-        <div className=''>
-            <input type='text' placeholder='Search...' />
+        <div className='App'>
+            <h1 className='title'>Search Bar App</h1>
+            <input type='text' placeholder='Search here' />
             {JSONDATA.map((val, key) => {
-                return <div>{val.first_name}</div>;
+                return (
+                    <div className='user' key={key}>
+                        <p>{val.first_name}</p>
+                    </div>
+                );
             })}
         </div>
     );
